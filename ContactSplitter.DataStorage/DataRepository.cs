3c3468787f4e0bbd6556503d0bad78716c539491
svsjo <weis_jonas@mail.de>
2023-04-30 10:24:10 +0200

    small changes

--- ContactSplitter.DataStorage/DataRepository.cs
+++ ContactSplitter.DataStorage/DataRepository.cs
@@ -33,23 +33,212 @@ namespace ContactSplitter.DataStorage
                 LetterSalutation = "Hallo",
                 Salutation = "Moin",
                 Title = "Absolvent"
-            }
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "M",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
+            new()
+            {
+                ForeName = "Jonas Noah",
+                LastName = "Schmid-Weis",
+                Gender = "Männlich",
+                LetterSalutation = "Sehr geehrter",
+                Salutation = "Sehr geehrter",
+                Title = "Prof. Dr.-Ing."
+            },
+            new()
+            {
+                ForeName = "Nonas Joah",
+                LastName = "Weis-Schmid",
+                Gender = "F",
+                LetterSalutation = "Sehr geehrte",
+                Salutation = "Sehr geehrte",
+                Title = "Dr.-Ing. net. rar."
+            },
+            new()
+            {
+                ForeName = "Arne",
+                LastName = "Amel",
+                Gender = "M",
+                LetterSalutation = "Hallo",
+                Salutation = "Moin",
+                Title = "Absolvent"
+            },
         };
 
         public static ObservableCollection<string> AllTitles { get; set; } = new ObservableCollection<string>()
         {
             "Dr.",
             "Prof.",
-            "Dip.-Ing.",
-            "Dr.",
-            "Prof.",
-            "Dip.-Ing.",
-            "Dr.",
-            "Prof.",
-            "Dip.-Ing.",
-            "Dr.",
-            "Prof.",
-            "Dip.-Ing."
+            "Dipl.-Ing.",
+            "Ing.",
+            "Mag.",
+            "Baron",
+            "Graf",
+            "Herzog",
+            "Fürst",
+            "Dipl.-Kfm.",
+            "RA",
+            "Notar"
         };
     }
 }
