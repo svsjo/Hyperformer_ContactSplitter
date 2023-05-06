5a3588b2e313f9c6de7e0d68c04b37cf851c513d
svsjo <weis_jonas@mail.de>
2023-05-06 14:25:16 +0200

    fix

--- GPTContactParser/GptContactParserPrompt.cs
+++ GPTContactParser/GptContactParserPrompt.cs
@@ -5,13 +5,13 @@ internal static class GptContactParserPrompt
     public static string Get()
     {
         const string dataRaw =
-            "Herr Prof. Dr. Russwurm, Winfried -> {Herr Professor; Dr. Prof.; Sehr geehrter Herr Professor; Winfried; Russwurm; M}";
+            "Herr Prof. Dr. Russwurm, Winfried -> {Herr Professor; Prof. Dr.; Sehr geehrter Herr Professor; Winfried; Russwurm; M}";
 
         const string task =
             "Du bist ein Parser zum Parsen von Personenmerkmalen. Das Ergebnis gibst du jeweils im Format: (Anrede; Titel; Begrüßung; Vorname; Nachname; Geschlecht) zurück." +
             "Bist du nicht sicher gibst du einfach den Wahrscheinlichsten Vorschlag an (nur ein Ergebnis). " +
             "Nutze zudem Logik und Weltwissen zum bestimmen des Geschlechtes. Gebe das Geschlecht mit M (Male) und F (Female) an." +
-            "Bei der Begrüßung nur den höchsten Titel verwenden und diesen bitte ausschreiben: 'Herr Prof. Dr.' wird z.B. zu 'Doktor'.";
+            "Bei der Begrüßung nur den höchsten Titel verwenden und diesen bitte ausschreiben: 'Herr Prof. Dr.' wird z.B. zu 'Professor'.";
 
 
         const string prompt =
