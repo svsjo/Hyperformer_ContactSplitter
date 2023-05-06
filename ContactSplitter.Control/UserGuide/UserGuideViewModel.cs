4979061b9626b389e1d11a865b13c2904a944a29
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2023-05-06 18:09:20 +0200

    .

--- ContactSplitter.Control/UserGuide/UserGuideViewModel.cs
+++ ContactSplitter.Control/UserGuide/UserGuideViewModel.cs
@@ -9,7 +9,7 @@ public class UserGuideViewModel : INotifyPropertyChanged
     private readonly ProjectSettings _projectSettings;
     private string _bedieneranleitung = "Diese Anwendung ist in verschiedene Seiten aufgeteilt.\r\n\n1. Parser:\r\nEine Eingabestring wird eingegeben, welcher geparste Ergebnisse zurückliefert.\r\nDie Felder können bearbeitet werden und schließlich muss auf Speichern gedrückt werden.\r\n\n2. Adressbuch:\r\nAlle gespeicherten Kontakte sind dort eingetragen. Sie können durch Doppelklick bearbeitet werden.\r\nSortierung wird durch Klick auf die Überschriften erreicht. Filterung durch das Textfeld unten.\r\nEin Button in jeder Zeile ermöglicht das Löschen.\r\n\n3. Anleitung: (hier)\r\n\n4. Einstellungen:\r\nAuf Seite 1 können Titel gelöscht und hinzugefügt werden.\r\nAuf Seite 2 können Präfixe gelöscht und hinzugefügt werden.\r\nAuf Seite 3 kann das Theme und der Parser ausgewählt werden.";
 
-    private string _bestPractices = "Unterschieden wird zwischen zwei Parsern, welche im folgenden weiter erklärt werden.\r\n\r\n1. ChatGPT: \r\nSetzt Internetverbindung vorraus und dauert einige Sekunden.\r\nStellt eine Anfrage an die API von ChatGPT, nutzt also ein Sprachmodell zur Lösung.\r\nVorteil ist, dass diese Version deutlich besser mit Grenzfällen umgehen kann.\r\nNachteil ist, dass die Ergebnisse bei jedem Aufruf variieren können und auch einfach Fehler enthalten.\r\n\r\n2. Offline:\r\nFunktioniert auch offline und liefert sofort ein Ergebnis.\r\nNutzt einen eigenen Parser, welcher nach fester Logik arbeitet.\r\nNachteil ist, dass die Eingabe in fester Form erfolgen muss und auf Grenzfälle schlechter reagieren kann. \r\nHerr/Frau - Titel (*) - Vornamen (*) - Präfix - Nachname (weitere Nachnamen nur mit -).\r\nEs werden nur Titel und Präfixe erkannt, welche auch in der Datenbank sind.";
+    private string _bestPractices = "Unterschieden wird zwischen zwei Parsern, welche im folgenden weiter erklärt werden.\r\n\r\n1. ChatGPT: \r\nSetzt Internetverbindung vorraus und dauert einige Sekunden.\r\n Nutzt die API des NLP Models GPT3.5 zur Lösung.\r\nVorteil ist, dass diese Version deutlich besser mit Grenzfällen umgehen kann.\r\nNachteil ist, dass die Ergebnisse bei jedem Aufruf variieren können und auch einfach Fehler enthalten.\r\n\r\n2. Offline:\r\nFunktioniert auch offline und liefert sofort ein Ergebnis.\r\nNutzt einen eigenen Parser, welcher nach fester Logik arbeitet.\r\nNachteil ist, dass die Eingabe in fester Form erfolgen muss und auf Grenzfälle schlechter reagieren kann. \r\nHerr/Frau - Titel (*) - Vornamen (*) - Präfix - Nachname (weitere Nachnamen nur mit -).\r\nEs werden nur Titel und Präfixe erkannt, welche auch in der Datenbank sind.";
 
     private string _developers = "Mitglieder: Jonathan Schwab, Felix Wochele, Seva Pypenko, Jonas Weis\nTemname: Hyperformer";
 
@@ -46,6 +46,17 @@ public class UserGuideViewModel : INotifyPropertyChanged
         set
         {
             _developers = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string ParserTypes
+    {
+        get => _parserTypes;
+        set
+        {
+            if (value == _parserTypes) return;
+            _parserTypes = value;
             OnPropertyChanged();
         }
     }
